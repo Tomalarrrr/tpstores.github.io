2a525cf15841a24b978db50eb318a8034219d7ab
Tomalarrrr <105936702+Tomalarrrr@users.noreply.github.com>
2023-02-10 19:26:28 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+
+document.cookie = "cookies=none";
+document.cookie = "sameSite=strict";
+
 !(function($) {
   "use strict";
 
